02ca7fdf0a41a8bf3335cb1318d08cf4f58bc9a2
Baptiste Langlade <baptouuuu@gmail.com>
2018-09-01 15:19:23 +0200

    add bootstrap function as an alternative to service container

---
+++ src/bootstrap.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\HttpTransport;
+
+use Innmind\Http\{
+    Translator\Response\Psr7Translator,
+    Factory\Header\Factories,
+};
+use GuzzleHttp\{
+    ClientInterface,
+    Client,
+};
+use Psr\Log\LoggerInterface;
+
+function bootstrap(): array
+{
+    $translator = new Psr7Translator(Factories::default());
+
+    return [
+        'guzzle' => static function(ClientInterface $client = null) use ($translator): Transport {
+            return new GuzzleTransport(
+                $client ?? new Client,
+                $translator
+            );
+        },
+        'cacth_guzzle_exceptions' => static function(Transport $transport) use ($translator): Transport {
+            return new CatchGuzzleBadResponseExceptionTransport(
+                $transport,
+                $translator
+            );
+        },
+        'logger' => static function(LoggerInterface $logger, string $level = null): callable {
+            return static function(Transport $transport) use ($logger, $level): Transport {
+                return new LoggerTransport(
+                    $transport,
+                    $logger,
+                    $level
+                );
+            };
+        },
+        'throw_client' => static function(Transport $transport): Transport {
+            return new ThrowOnClientErrorTransport($transport);
+        },
+        'throw_server' => static function(Transport $transport): Transport {
+            return new ThrowOnServerErrorTransport($transport);
+        },
+    ];
+}
---
+++ tests/BootstrapTest.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\HttpTransport;
+
+use Innmind\HttpTransport\{
+    GuzzleTransport,
+    CatchGuzzleBadResponseExceptionTransport,
+    LoggerTransport,
+    ThrowOnClientErrorTransport,
+    ThrowOnServerErrorTransport,
+};
+use function Innmind\HttpTransport\bootstrap;
+use GuzzleHttp\ClientInterface;
+use Psr\Log\LoggerInterface;
+use PHPUnit\Framework\TestCase;
+
+class BootstrapTest extends TestCase
+{
+    public function testBootstrap()
+    {
+        $transports = bootstrap();
+        $guzzle = $transports['guzzle'];
+        $catchExceptions = $transports['cacth_guzzle_exceptions'];
+        $log = $transports['logger']($this->createMock(LoggerInterface::class));
+        $throwClient = $transports['throw_client'];
+        $throwServer = $transports['throw_server'];
+
+        $this->assertInstanceOf(GuzzleTransport::class, $guzzle());
+        $this->assertInstanceOf(GuzzleTransport::class, $guzzle(
+            $this->createMock(ClientInterface::class)
+        ));
+        $this->assertInstanceOf(
+            CatchGuzzleBadResponseExceptionTransport::class,
+            $catchExceptions($guzzle())
+        );
+        $this->assertInternalType('callable', $log);
+        $this->assertInstanceOf(LoggerTransport::class, $log($guzzle()));
+        $this->assertInternalType('callable', $throwClient);
+        $this->assertInstanceOf(ThrowOnClientErrorTransport::class, $throwClient($guzzle()));
+        $this->assertInternalType('callable', $throwServer);
+        $this->assertInstanceOf(ThrowOnServerErrorTransport::class, $throwServer($guzzle()));
+    }
+}
