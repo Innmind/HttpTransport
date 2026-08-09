947e7bba12e0e68b9ca50c2cf1db961da507fbb3
Baptiste Langlade <baptiste.langlade@hey.com>
2026-08-09 16:50:26 +0200

    httpbin requires a user agent now

--- tests/CurlTest.php
+++ tests/CurlTest.php
@@ -19,6 +19,8 @@ use Innmind\Http\{
     Method,
     ProtocolVersion,
     Headers,
+    Header,
+    Header\Value,
     Header\Date,
     Header\Location,
 };
@@ -503,6 +505,9 @@ class CurlTest extends TestCase
                 Url::of("https://http$server.org/delay/2"),
                 Method::get,
                 ProtocolVersion::v11,
+                Headers::of(
+                    Header::of('User-Agent', Value::of('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.5.2 Safari/605.1.15')),
+                )
             );
 
             $result = ($this->curl)($request)->match(
